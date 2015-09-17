2be4cbc2dae8a4cece5307a19c9d7c7414676eec
alvin.jeremie <alvin.jeremie@R21005.BTSSIO.local>
2015-09-17 10:41:01 +0200

    MAJ ListeLivres

--- view/ListeLivres.php
+++ view/ListeLivres.php
@@ -3,17 +3,17 @@
     <head>
     </head>
 <body>
-    <?php
-    $liste = "<table class='col-md-5'>
-            <tr><td>Titre</td><td>Auteur</td></tr>";
+    
+    <table class='col-md-5'>
+            <tr><td>Titre</td><td>Auteur</td></tr>
 
-                   foreach ($livres as $titre => $livre)
+                 <?php  foreach ($livres as $titre => $livre)
                     {
                             echo '<tr><td><a href="index.php?action=livre&amp;livre='.$livre->getTitre().'">'.$livre->getTitre().'</a></td><td>'.$livre->getAuteur().'</td><td>'.'<img src="Images/"'.$livre->getImage().'></img>'.'</td></tr>';
                     }
-
+?>
             
-    "</table>";
-    ?>
+    </table>
+    
 </body>
 </html>
