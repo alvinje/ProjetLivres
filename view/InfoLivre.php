48cbe94968cfbe35b8b3793e3928044427c09cc3
lepine.remi <lepine.remi@R21006.BTSSIO.local>
2015-09-24 11:39:19 +0200

    Merge origin/Developpement into Developpement
    Conflicts:
    	view/Header.php

--- view/InfoLivre.php
+++ view/InfoLivre.php
@@ -14,12 +14,19 @@
                             echo '<h3>Titre: ' . $livre->getTitre() . '<br/>';
                         echo '</h3>';
                         echo 'Auteur: ' . $livre->getAuteur() . '<br/>';
-                        echo 'Description: ' . $livre->getDescription() . '<br/>';
-                        echo'<br/><a href="index.php?action=livres"><button class="btn btn-primary">Retour</button></a>';
+                        echo 'Description: ' . $livre->getDescription() . '<br/><br/>';
+                        echo'<a href="index.php?action=livres"><button class="btn btn-primary">Retour</button></a><br/><br/>';
+                        echo'<a href="index.php?action=livres&amp;del='.$livre->getTitre().'"><button class="btn btn-danger">Supprimer livre</button></a><br/>';
                     echo'</div>';
                     echo'<div class="col-xs-7"></div>';
                 echo'</div>';
                 echo'</div>';
+                
+                if(isset($_GET['del']))
+                {
+                    $livres = $this->_connexionDb->requete('DELETE from livre WHERE titre="'.$_GET['del'].'"');
+                    header('Location: index.php');
+                }
            
     ?>
         
