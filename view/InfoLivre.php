b4f725956a4f2a4030e22fb70b49dd7953f6b0b8
alvin.jeremie <alvin.jeremie@R21005.BTSSIO.local>
2015-09-24 09:25:13 +0200

    Mise en page InfoLivre

--- view/InfoLivre.php
+++ view/InfoLivre.php
@@ -4,12 +4,18 @@
 <body>
 
     <?php 
-            echo'<div class = "morph pic">';
+            echo '<div class="col-md-7 col-xs-12 portfolio-item" >';
+            echo'<div class = "morph1 pic">';
             echo  '<img src="Images/'.$livre->getImage().'"></img><br />';
             echo'</div>';
+            echo '<div class = "col-md-6 col-xs-12 centrer infoLivre"style = "position: fixed;"';
+            echo '<h3>';
             echo 'Titre:' . $livre->getTitre() . '<br/>';
+            echo '</h3>';
             echo 'Auteur:' . $livre->getAuteur() . '<br/>';
             echo 'Description:' . $livre->getDescription() . '<br/>';
+            echo'</div>';
+            echo'</div>';
 
     ?>
     <a href="index.php?action=livres"><button>Retour</button></a>
